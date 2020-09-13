73e672cf6a4fee35c00371f2c42ffbfcce2dde49
拉拉醬 <lala.lee.goo@gmail.com>
2020-09-13 10:42:06 +0800

    parameters - global level

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,4 +6,10 @@ setCompodocJson(docJson);
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  backgrounds: {
+    values: [
+      { name: 'global-red', value: 'red' },
+      { name: 'global-green', value: 'green' },
+    ],
+  },
 }
